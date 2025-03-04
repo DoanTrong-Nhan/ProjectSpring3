7accfc792cc39b8259ba80c4407670039a7b837e
nhantacb <nhantacbvn@gmail.com>
2025-03-04 15:53:30 +0700

    Encoding Bcrypt

---
+++ src/main/java/spring/api/springcore/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package spring.api.springcore.controller;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.api.springcore.dto.request.ApiResponse;
+import spring.api.springcore.dto.request.AuthenticationRequest;
+import spring.api.springcore.dto.request.response.AuthenticationResponse;
+import spring.api.springcore.service.AuthenticationService;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationController {
+    AuthenticationService authenticationService;
+
+    @PostMapping("/log-in")
+    public ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        boolean result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(AuthenticationResponse.builder()
+                        .authenticated(result)
+                        .build())
+                .build();
+    }
+}
+
--- src/main/java/spring/api/springcore/service/UserService.java
+++ src/main/java/spring/api/springcore/service/UserService.java
@@ -4,6 +4,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import spring.api.springcore.dto.request.UserCreationRequest;
 import spring.api.springcore.dto.request.UserUpdateRequest;
@@ -30,6 +32,8 @@ public class UserService {
             throw new RuntimeException("ErrorCode.USER_EXISTED");
 
         User user = userMapper.toUser(request);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         return userRepository.save(user);
     }
